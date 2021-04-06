964998f5d29fac6fc6b7a36faf897921c5e739a2
Mingyu Kim <68273065+MingyuKim-2933@users.noreply.github.com>
2021-04-06 22:08:48 +0900

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform childTransform; // 움직일 자식 게임 오브젝트의 트랜스폼
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 자신의 전역 위치를 (0, -1, 0)으로 변경
+        transform.position = new Vector3(0, -1, 0);
+        // 자식의 지역 위치를 (0, 2, 0)으로 변경
+        childTransform.localPosition = new Vector3(0, 2, 0);
+
+        // 자신의 전역 회전을 (0, 0, 30)으로 변경
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 30));
+        // 자식의 지역 회전을 (0, 60, 0)으로 변경
+        childTransform.localRotation = Quaternion.Euler(new Vector3(0, 60, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 위쪽 방향키를 누르면 초당 (0, 1, 0)의 속도로 평행이동
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(transform.up * Time.deltaTime);
+        }
+
+        // 아래쪽 방향키를 누르면 초당 (0, -1, 0)의 속도로 평행이동
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(transform.up * -1 * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            // 왼쪽 방향키를 누르면
+            // 자신을 초당 (0, 0, 180) 회전
+            transform.Rotate(new Vector3(0, 0, 180) * Time.deltaTime);
+            // 자식을 초당 (0, 180, 0) 회전
+            childTransform.Rotate(new Vector3(0, 180, 0) * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            // 오른쪽 방향키를 누르면
+            // 자신을 초당 (0, 0, -180) 회전
+            transform.Rotate(new Vector3(0, 0, -180) * Time.deltaTime);
+            // 자식을 초당 (0, -180, 0) 회전
+            childTransform.Rotate(new Vector3(0, -180, 0) * Time.deltaTime);
+        }                
+    }
+}
